1042ea1955ac748c06d9cfe066b36c64bab4d568
Sedef YILDIZ <164248954+funwithpeeves@users.noreply.github.com>
2025-01-30 11:29:41 +0300

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Sedef <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+        <p className="hero_tag text-gray_gradient">Ürünler ve Marka Oluşturma</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
@@ -57,7 +57,7 @@ const Hero = () => {
 
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
-          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+          <Button name="Beraber çalışalım" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
       </div>
     </section>
