9a71f274d79b0c6282fbdd6f1f0db9cf7efef147
Sedef YILDIZ <164248954+funwithpeeves@users.noreply.github.com>
2025-01-30 11:38:59 +0300

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -72,15 +72,14 @@ const Contact = () => {
         <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
 
         <div className="contact-container">
-          <h3 className="head-text">Let's talk</h3>
+          <h3 className="head-text">Konuşalım</h3>
           <p className="text-lg text-white-600 mt-3">
-            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
-            life, I’m here to help.
+            Yeni bir web sitesi oluşturmak, mevcut platformunuzu iyileştirmek veya benzersiz bir projeyi hayata geçirmek istiyorsanız, size yardımcı olmak için buradayım.
           </p>
 
           <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
             <label className="space-y-3">
-              <span className="field-label">Full Name</span>
+              <span className="field-label">İsim</span>
               <input
                 type="text"
                 name="name"
@@ -93,7 +92,7 @@ const Contact = () => {
             </label>
 
             <label className="space-y-3">
-              <span className="field-label">Email address</span>
+              <span className="field-label">Email</span>
               <input
                 type="email"
                 name="email"
@@ -106,7 +105,7 @@ const Contact = () => {
             </label>
 
             <label className="space-y-3">
-              <span className="field-label">Your message</span>
+              <span className="field-label">Mesajınız</span>
               <textarea
                 name="message"
                 value={form.message}
@@ -114,7 +113,7 @@ const Contact = () => {
                 required
                 rows={5}
                 className="field-input"
-                placeholder="Share your thoughts or inquiries..."
+                placeholder="Fikirlerinizi veya sorularınızı paylaşabilirsiniz..."
               />
             </label>
 
